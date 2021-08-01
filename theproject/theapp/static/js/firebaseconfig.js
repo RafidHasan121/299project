c9bbe92b3b09c38c426d9735729f6155d64da204
RafidHasan121 <rafid.hasan@northsouth.edu>
2021-08-01 20:12:10 +0600

    some basic setups

---
+++ theproject/theapp/static/js/firebaseconfig.js
@@ -0,0 +1,21 @@
+//The core Firebase JS SDK is always required and must be listed first 
+src="https://www.gstatic.com/firebasejs/8.8.1/firebase-app.js"
+
+//TODO: Add SDKs for Firebase products that you want to use
+//     https://firebase.google.com/docs/web/setup#available-libraries
+src="https://www.gstatic.com/firebasejs/8.8.1/firebase-analytics.js"
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCqa4-n6rAH4w16W7k0ZZBhW6nKqaOyYgU",
+    authDomain: "project-78015.firebaseapp.com",
+    projectId: "project-78015",
+    storageBucket: "project-78015.appspot.com",
+    messagingSenderId: "42607330346",
+    appId: "1:42607330346:web:4ad5d92b9c9f49f7c1ef53",
+    measurementId: "G-KSMCSSMRW4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
